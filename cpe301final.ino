8af3c91ade535fc62332ff84fd32a05cd6b398bf
Chloe <112997314+chloeharger@users.noreply.github.com>
2024-12-02 11:43:03 -0800

    Update clock, temp, humidity

--- cpe301final.ino
+++ cpe301final.ino
@@ -15,20 +15,46 @@ CPE 301 Final Project main code file
 // Temp/Humidity Sensor Setup:
 #include <dht.h>
 dht DHT;
-#define DHT11_PIN 1
+#define DHT11_PIN 7
 
 // LCD Display Setup:
 #include <LiquidCrystal.h>
-const int RS = 11, EN = 12, D4 = 2, D5 = 3, D6 = 4, D7 = 5;
+const int RS = 12, EN = 13, D4 = 2, D5 = 3, D6 = 4, D7 = 5;
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+
+// Setup for using millis()
+unsigned long int previousMillis=0;
+const long int interval = 1000;
+
+// Setup for clock:
+#include <RTClib.h>
+RTC_DS3231 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 void setup(){
     U0init(9600);
     lcd.begin(16, 2);
+    rtc.begin();
 }
 
 void loop(){
-    
+  
+    //Display temp and humidity levels
+    unsigned long currentMillis = millis();
+
+    if(currentMillis - previousMillis >= interval){
+        previousMillis = currentMillis;
+        
+        int chk = DHT.read11(DHT11_PIN);
+        lcd.setCursor(0,0);
+        lcd.print("Temp=");
+        lcd.setCursor(7, 0);
+        lcd.print(DHT.temperature);
+        lcd.setCursor(0,1);
+        lcd.print("Humidity=");
+        lcd.setCursor(9, 1);
+        lcd.print(DHT.humidity);
+    }
 }
 
 //UART Functions:
@@ -52,11 +78,28 @@ void U0putchar(unsigned char U0pdata){
     *myUDR0 = U0pdata;
 }
 
-
 //(can delete these comments once code is started)
 // Set up water sensor
-// Set up temperature + humidity module
+
 // Fan motor (based on temperature levels)
 // Set up stepper motor for vent
 // On/off button
-// Set date+time when system turns on or off
+
+// Set date+time when system turns on or off:
+void displayClock(){
+    DateTime now = rtc.now();
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+}
