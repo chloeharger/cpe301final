5b2615191f33fe3c9a9bac7475a537a458de52c5
Chloe <112997314+chloeharger@users.noreply.github.com>
2024-12-02 18:04:30 +0000

    added comments

--- cpe301final.ino
+++ cpe301final.ino
@@ -2,3 +2,13 @@
 Chloe Harger and Genesis Mendoza
 CPE 301 Final Project main code file
 */
+
+// 
+
+//(can delete these comments once code is started)
+// Set up water sensor
+// Set up temperature + humidity module
+// Fan motor (based on temperature levels)
+// Set up stepper motor for vent
+// On/off button
+// Set date+time when system turns on or off
